9aa4509223e703f039490d396fe325e553c94abd
liisieenmaa <eenmaaliisi@gmail.com>
2022-09-21 16:57:17 +0300

    Liisi selecting month and year

--- src/views/BudgetView.vue
+++ src/views/BudgetView.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
+      <div class="dropdown">
+          <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-on:click="findDateData">
+              Kuu
+          </button>
+          <div v-for="planninginfo in planningInfos" class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="#">{{planninginfo.month}}</a>
+
+          </div>
+      </div>
+      <div>
+
+      </div>
 
     <h1> Minu eelarve</h1><br>
     <h2> Tulud </h2>
@@ -28,11 +40,27 @@ export default {
   name: "BudgetView",
   components: {SetupIncomeWithData, SetupExpenseWithData},
   data() {
-    return {}
+    return {
+        userId: sessionStorage.getItem('userId'),
+        planningInfos: {
+            year: 0,
+            month: 0
+        }
+    }
 
   },
 
-  methods: {},
+  methods: {
+      findDateData: function () {
+          this.$http.get("/budget/planning/month/all")
+              .then(response => {
+                  console.log(response.data)
+              }).catch(error => {
+              console.log(error)
+          })
+      }
+      },
+
   mounted() {
 
   }
